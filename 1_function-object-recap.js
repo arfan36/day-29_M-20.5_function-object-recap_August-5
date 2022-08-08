6398ec69301726d3ddcaf64c401000091c428a7a
arfan36 <arfanur36@gmail.com>
2022-08-08 07:48:21 +0600

    20_5-1 JavaScript concepts and...

---
+++ 1_function-object-recap.js
@@ -0,0 +1,29 @@
+/* 
+-------------------------------------
+    Six Core Concepts
+    1. variable (declare, naming conventin, variable types, primitive, math operations)
+    2. Conditionals (comparison: >, <, >=, <=, ===, !==, if-else, if-else if-else)
+    3. Array (declare, index, get values, set values, indexOf, push, pop)
+    4. Loop (for loop, while loop, break, continue)
+    5. function (declare, parameters, return, call, use the returned value from a function)
+    6. Object (declare, properties, keys, values, get prop value, set value, loop object properties)
+-------------------------------------
+ */
+
+
+/* 
+    1. google: function declaration vs function expression
+    2. google: arrow function
+    3. google: loop vs function
+    4. google: array vs object
+    5. google: integer vs float
+    6. google: == vs ===
+    7. google: differences between: var, let, const
+ */
+
+/* 
+    1. celcious -----> fahrenheit
+    2. fahrenheit -------> celcious
+    3. Simple Interest: principal, interest rate, time ---> interest amount
+    4. complex Interest: 
+ */
